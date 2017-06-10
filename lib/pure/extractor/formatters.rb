cb70230124337d47588bb08f150bc0224a866d08
Stephen Robinson <stephen@stephen-robinson.co.uk>
2017-06-10 19:50:12 +0100

    Add formatters module file

---
+++ lib/pure/extractor/formatters.rb
@@ -0,0 +1,11 @@
+require 'pure/extractor/formatters/resource'
+require 'pure/extractor/formatters/organisation_unit'
+require 'pure/extractor/formatters/person'
+
+module Pure
+  module Extractor
+    module Formatters
+
+    end
+  end
+end
